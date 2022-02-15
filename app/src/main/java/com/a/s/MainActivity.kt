be4f64969791a741a8bf3f5a77af3ec067520908
lijunjie <lijunjie@windimg.com>
2022-02-15 15:03:22 +0800

    增加说明文档

--- app/src/main/java/com/a/s/MainActivity.kt
+++ app/src/main/java/com/a/s/MainActivity.kt
@@ -2,10 +2,7 @@ package com.a.s
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.a.findfragment.ListActivity
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : ListActivity() {
 }
